5d7495726d0219bec10b1bc46a2c6c05a9303fa6
mankek <krystal.manke@gmail.com>
2018-09-25 11:16:40 -0500

    Basics of ant interaction down; see TODO in methods

--- App/methods.py
+++ App/methods.py
@@ -1,9 +1,14 @@
 #!/usr/bin/python
 import random
 
+# TODO: rather than hard-coded colors, allow for input of array of colors
+# TODO: Introduce random death occurrence
+
+
 ants = {}
 
 
+# Initializes the input number of ants with specified colors
 def get_ants(num_blue, num_green):
     for s in range(0, num_blue + 1):
         ants[s] = {'position': 0, 'color': 'blue', 'colors': []}
@@ -12,6 +17,7 @@ def get_ants(num_blue, num_green):
     return print('get_ants done!')
 
 
+# Each cycle each ant moves to a random position; checks if ants 'meet' (are at same position)
 def cycle_ants(cycles, position_range):
     for i in range(0, cycles + 1):
         for s in ants:
@@ -25,9 +31,18 @@ def cycle_ants(cycles, position_range):
                         ants[t]['colors'].append(ants[u]['color'])
                         ants[u]['colors'].append(ants[t]['color'])
         meeting_results()
-    return print('cycle_ants done')
+    # Show ending distribution of ants
+    Green = 0
+    Blue = 0
+    for v in ants:
+        if ants[v]['color'] == 'green':
+            Green += 1
+        elif ants[v]['color'] == 'blue':
+            Blue += 1
+    return print("Blue: " + str(Blue) + "\n" + "Green: " + str(Green))
 
 
+# If ants have met, records color of ant that was met
 def meeting_results():
     for i in ants:
         green = 0
@@ -38,30 +53,33 @@ def meeting_results():
             elif s == 'blue':
                 blue += 1
         resp = color_response(green, blue, i)
-        print(str(i) + ' Green: ' + str(green))
-        print(str(i) + ' Blue: ' + str(blue))
-        print(resp)
-    return print('meeting_results done!')
+        # print(str(i) + ' Green: ' + str(green))
+        # print(str(i) + ' Blue: ' + str(blue))
+        if resp != '':
+            print(resp)
 
 
+# Determines if ant changes color in response to ants it's met
 def color_response(green, blue, ant_id):
     if (green >= 5) and (blue < 5):
         if ants[ant_id]['color'] == 'blue':
-            return 'color unchanged'
+            return ''
         else:
+            print(str(ant_id) + ' original color: ' + str(ants[ant_id]['color']))
             ants[ant_id]['color'] = 'blue'
             return 'color changed to blue'
     elif (green < 5) and (blue >= 5):
         if ants[ant_id]['color'] == 'green':
-            return 'color unchanged'
+            return ''
         else:
+            print(str(ant_id) + ' original color: ' + str(ants[ant_id]['color']))
             ants[ant_id]['color'] = 'green'
             return 'color changed to green'
     else:
-        return 'color unchanged'
+        return ''
 
 
-get_ants(num_blue=4, num_green=6)
-cycle_ants(5, 10)
+get_ants(num_blue=10, num_green=20)
+cycle_ants(20, 10)
 
 
